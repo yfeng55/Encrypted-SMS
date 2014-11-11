fa603eb7d6ddac8beeead8bbd0e16a88b02ae4b7
Yijie Feng <yijiefeng@gmail.com>
2014-11-10 23:59:43 -0800

    contact list and style changes

--- gen/com/example/encryptedsms/R.java
+++ gen/com/example/encryptedsms/R.java
@@ -40,9 +40,9 @@ public final class R {
         public static final int layer_padding=0x7f050003;
     }
     public static final class drawable {
-        public static final int flat_selector=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int messagebubble=0x7f020002;
+        public static final int edittextlines=0x7f020000;
+        public static final int flat_selector=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
         public static final int rect_normal=0x7f020003;
         public static final int rect_pressed=0x7f020004;
     }
@@ -102,14 +102,16 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f070001;
+        public static final int ContactListLabel=0x7f070006;
         /**  CUSTOM THEME 
          */
         public static final int CustomActionBarTheme=0x7f070002;
-        public static final int EditText=0x7f070006;
+        public static final int EditText=0x7f070008;
         /**  ActionBar styles 
  <item name="android:icon">@android:color/transparent</item> 
          */
         public static final int MyActionBar=0x7f070003;
+        public static final int SolidLine=0x7f070007;
         /**  VIEW STYLES 
          */
         public static final int TitleLabel=0x7f070005;
--- src/com/example/encryptedsms/ContactListFragment.java
+++ src/com/example/encryptedsms/ContactListFragment.java
@@ -3,6 +3,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.provider.ContactsContract.Contacts;
 import android.support.v4.app.ListFragment;
 import android.support.v4.app.LoaderManager.LoaderCallbacks;
@@ -22,53 +23,73 @@ public class ContactListFragment extends ListFragment implements LoaderCallbacks
 
 		// create adapter once
 		Context context = getActivity();
-		int layout = android.R.layout.simple_list_item_1;
-		Cursor c = null; // there is no cursor yet
-		int flags = 0; // no auto-requery! Loader requeries.
-		mAdapter = new SimpleCursorAdapter(context, layout, c, FROM, TO, flags);
+		Cursor c = null; //no cursor yet
+		
+		mAdapter = new SimpleCursorAdapter(context, android.R.layout.simple_list_item_1, c, FROM, TO, 0);
 	}
 
+	
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
-
-		// each time we are started use our listadapter
+		
 		setListAdapter(mAdapter);
-		// and tell loader manager to start loading
 		getLoaderManager().initLoader(0, null, this);
 	}
 
+	
 	// columns requested from the database
-	private static final String[] PROJECTION = { Contacts._ID, // _ID is always
-																// required
-			Contacts.DISPLAY_NAME_PRIMARY // that's what we want to display
+	private static final String[] PROJECTION = { ContactsContract.CommonDataKinds.Phone._ID, // _ID is always required
+		ContactsContract.Contacts.DISPLAY_NAME,
+		ContactsContract.CommonDataKinds.Phone.NUMBER
 	};
 
-	// and name should be displayed in the text1 textview in item layout
-	private static final String[] FROM = { Contacts.DISPLAY_NAME_PRIMARY };
-	private static final int[] TO = { android.R.id.text1 };
+	
+	private static final String[] FROM = { ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+		ContactsContract.CommonDataKinds.Phone.NUMBER
+	};
+	
+	// Name and phone number are displayed in the text1 textview in item layout
+	private static final int[] TO = { android.R.id.text1};
 
+	
 	@Override
 	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
 
 		// load from the "Contacts table"
-		Uri contentUri = Contacts.CONTENT_URI;
-
-		// no sub-selection, no sort order, simply every row
-		// projection says we want just the _id and the name column
-		return new CursorLoader(getActivity(), contentUri, PROJECTION, null,
-				null, null);
+		Uri contentUri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+		
+		String sortOrder = ContactsContract.Contacts.DISPLAY_NAME + " ASC";
+		return new CursorLoader(getActivity(), contentUri, PROJECTION, null, null, sortOrder);
 	}
+	
 
 	@Override
 	public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
 		// Once cursor is loaded, give it to adapter
 		mAdapter.swapCursor(data);
 	}
+	
 
 	@Override
 	public void onLoaderReset(Loader<Cursor> loader) {
 		// on reset take any old cursor away
 		mAdapter.swapCursor(null);
 	}
+	
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
